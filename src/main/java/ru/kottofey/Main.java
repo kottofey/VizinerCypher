7a921363248f299a5f0c4376e019f3bea56a3d97
kottofey <romeokontakt@gmail.com>
2023-05-27 19:26:09 +0300

    2023-05-27 19:23LT Debug mode is set by separate method
    Now debug is switched off by default. IDEA run configuration has --debug option for command line so there is debug on in IDEA and compiled jar has it off.
    
    Signed-off-by: kottofey <romeokontakt@gmail.com>

--- src/main/java/ru/kottofey/Main.java
+++ src/main/java/ru/kottofey/Main.java
@@ -1,28 +1,11 @@
 package ru.kottofey;
 
 public class Main {
-	/*
-	 TODO: Custom CodeTable with user entering first line of table,
-	 		i.e. allowed characters. Need to check against user
-	 		input of phrase and key for correct input
-	 TODO: Add more different ciphers
-	 TODO: Show cipher type in Hello Menu when various ciphers are implemented
-	 TODO: Input from file and output to file
-	 TODO: Make exe with cmd line arguments for phrase and key
-	 TODO: Probably better with HashMap?
-	 TODO: Wrap it into interface somehow...
-	 TODO: Change menu logic, must be all in one place
-	*/
 
 	static boolean DEBUG = false;
 
 	public static void main(String[] args) {
-
-		if (args.length != 0 && (args[0].equals("--debug") || args[0].equals("-d"))) {
-			DEBUG = true;
-			System.out.println("---------========= DEBUG MODE !!!! =========---------\n" +
-					"--=== SWITCH OFF IF RUNNING OUTSIDE OF IDEA !!! ===--");
-		}
+		isDebug(args);
 
 		VizinerCipher transcoder;
 		CodeTable.makeCodeTable();
@@ -30,6 +13,13 @@ public class Main {
 		do {
 			transcoder = new VizinerCipher();
 		} while (!transcoder.getMode().equals("exit"));
+	}
 
+	private static void isDebug(String[] args) {
+		if (args.length != 0 && (args[0].equals("--debug") || args[0].equals("-d"))) {
+			DEBUG = true;
+			System.out.println("---------========= DEBUG MODE !!!! =========---------\n" +
+					"--=== SWITCH OFF IF RUNNING OUTSIDE OF IDEA !!! ===--");
+		}
 	}
 }
